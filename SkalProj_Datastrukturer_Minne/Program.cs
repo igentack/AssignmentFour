1813d594aa37f7c6ecc9c96daaf0186f48965ae9
satisfly <vilkenbraadress@gmail.com>
2022-11-19 17:26:05 +0100

    Add project files.

---
+++ SkalProj_Datastrukturer_Minne/Program.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SkalProj_Datastrukturer_Minne
+{
+    class Program
+    {
+        /// <summary>
+        /// The main method, vill handle the menues for the program
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("Please navigate through the menu by inputting the number \n(1, 2, 3 ,4, 0) of your choice"
+                    + "\n1. Examine a List"
+                    + "\n2. Examine a Queue"
+                    + "\n3. Turn around a word"
+                    + "\n4. CheckParanthesis"
+                    + "\n5. Recursive Fibonacci"
+                    + "\n6. Iterative Fibonacci"
+                    + "\n0. Exit the application");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        ExamineList();
+                        break;
+                    case '2':
+                        ExamineQueue();
+                        break;
+                    case '3':
+                        ExamineStack();
+                        break;
+                    case '4':
+                        CheckParanthesis();
+                        break;
+                    case '5':
+                        RecursiveFibonacci();
+                        break;
+                    case '6':
+                        IterativeFibonacci();
+                        break;
+                    /*
+                     * Extend the menu to include the recursive 
+                     * and iterative exercises.
+                     */
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure List
+        /// </summary>
+        static void ExamineList()
+        {
+            static void Tess()
+            {
+                Console.WriteLine($"testar");
+            }
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch statement with cases '+' and '-'
+             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
+             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
+             * In both cases, look at the count and capacity of the list
+             * As a default case, tell them to use only + or -
+             * Below you can see some inspirational code to begin working.
+            */
+
+            //string input = Console.ReadLine();
+            //char nav = input[0];
+            //string value = input.substring(1);
+            //theList.Add(value);
+
+            //switch(nav){...}
+            List<string> theList = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("Hi you could either push + OR - together with a name \n or you could just choose + OR -, your choice"
+                    + "\n1. + and a name (or just +)"
+                    + "\n2. - and a name to remove (or just -)"
+                    //+ "\n3. just +"
+                    //+ "\n4. just -"
+                    + "\n0. Exit");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        Console.Write("Skriv in namn: ");
+                        string name = Console.ReadLine();
+                        //string value = input.substring(1);
+                        if (name.Length == 0)
+                            name = "harry";
+                        theList.Add(name);
+
+                        foreach (var item in theList)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        Console.WriteLine(theList.Count);
+                        Tess();
+                        Console.WriteLine(theList.Capacity);
+                        Console.WriteLine();
+                        break;
+                    case '2':
+                        theList.RemoveAt(0);
+                        Console.WriteLine(theList.Count);
+                        Console.WriteLine(theList.Capacity);
+                        Console.WriteLine();
+                        break;
+                    case '3':
+                        ExamineStack();
+                        break;
+                    case '4':
+                        CheckParanthesis();
+                        break;
+                    /*
+                     * Extend the menu to include the recursive 
+                     * and iterative exercises.
+                     */
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure Queue
+        /// </summary>
+        static void ExamineQueue()
+        {
+            /*
+             * Loop this method untill the user inputs something to exit to main menue.
+             * Create a switch with cases to enqueue items or dequeue items
+             * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
+            */
+
+            Queue<string> theQue = new Queue<string>();
+            while (true)
+            {
+                Console.WriteLine("Hej och välkommen till Ica \n"
+                    + "\n1. Ställ dig i kön."
+                    + "\n2. Tack för besöket - hoppas godiset är gott :)"
+                    + "\n3. När är det min tur?"
+                    //+ "\n4. just -"
+                    + "\n0. Exit");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        Console.Write("Skriv in namn: ");
+                        string name = Console.ReadLine();
+                        //string value = input.substring(1);
+                        if (name.Length == 0)
+                            name = "harry";
+                        theQue.Enqueue($"{name} - tar det lång tid?");
+
+                        foreach (var item in theQue)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        Console.WriteLine(theQue.Count);
+                        //Tess();
+                        //Console.WriteLine(theQue.Capacity);
+                        Console.WriteLine();
+                        break;
+                    case '2':
+                        if (theQue.Count() == 0)
+                            Console.WriteLine("Du är först i kön :)");
+                        break;
+                        theQue.Dequeue();
+                        Console.WriteLine(theQue.Count);
+                        //Console.WriteLine(theQue.Capacity);
+                        Console.WriteLine();
+                        break;
+                    case '3':
+                        theQue.Peek();
+                        Console.WriteLine("{0} Det blir strax din tur", theQue.Peek());
+                        break;
+                    case '0':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Examines the datastructure Stack
+        /// </summary>
+        static void ExamineStack()
+        {
+            /*
+             * Loop this method until the user inputs something to exit to main menue.
+             * Create a switch with cases to push or pop items
+             * Make sure to look at the stack after pushing and and poping to see how it behaves
+            */
+
+            Stack stack = new Stack();
+            string drow = "";
+            Console.WriteLine("\nHej skriv in ett valfritt ord.");
+            while (true)
+            {
+                Console.WriteLine(""
+                    + "\n1. Välj ett ord"
+                    + "\n0. Tillbaka till huvudmenyn");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                switch (input)
+                {
+                    case '1':
+                        Console.Write("Skriv in här: ");
+                        string word = Console.ReadLine();
+                        for (int i = 0; i < word.Length; i++)
+                        {
+                            stack.Push(word[i]);
+                        }
+                        drow = String.Join("", stack.ToArray());
+                        Console.WriteLine();
+                        Console.WriteLine(drow);
+                        break;
+                    case '0':
+                        Main();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+
+        static void CheckParanthesis()
+        {
+            /*
+             * Use this method to check if the paranthesis in a string is Correct or incorrect.
+             * Example of correct: (()), {}, [({})],  List<int> list = new List<int>() { 1, 2, 3, 4 };
+             * Example of incorrect: (()]), [), {[()}],  List<int> list = new List<int>() { 1, 2, 3, 4 );
+             */
+            Stack<char> parStack = new Stack<char>();
+            parStack.Push('*');
+            Console.WriteLine("\nVälformade paranteser?");
+            while (true)
+            {
+                Console.WriteLine(""
+                    + "\n1. Prova"
+                    + "\n0. Tillbaka till huvudmenyn");
+                char input = ' '; //Creates the character input to be used with the switch-case below.
+                try
+                {
+                    input = Console.ReadLine()![0]; //Tries to set input to the first char in an input line
+                }
+                catch (IndexOutOfRangeException) //If the input line is empty, we ask the users for some input.
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter some input!");
+                }
+                //object paramInput = null;
+                switch (input)
+                {
+                    case '1':
+                        Console.Write("Skriv in parantessträngen: ");
+
+                        string inputParenthesis = Console.ReadLine();
+                        char[] stringToChar = inputParenthesis.ToCharArray();
+
+                        for (int i = 0; i < stringToChar.Length; i++)
+                        {
+                            if (stringToChar.Contains('(') || stringToChar.Contains('{') || stringToChar.Contains('['))
+                            {
+                                parStack.Push(stringToChar[0]);
+                            }
+                            if (parStack.Peek() == '(' && stringToChar.Contains(')') || (parStack.Peek() == '{' && stringToChar.Contains('}') || (parStack.Peek() == '[' && stringToChar.Contains(']'))))
+                            {
+                                parStack.Pop();
+                            }
+
+                        }
+                        if (parStack.Count == 1)
+                        {
+                            Console.WriteLine("Det finns en balans!");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No balance!");
+                            break;
+                        }
+                    case '0':
+                        Main();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter some valid input (0, 1, 2, 3, 4)");
+                        break;
+                }
+            }
+        }
+        static void RecursiveFibonacci()
+        {
+            Console.WriteLine("Time for föda");
+        }
+        static void IterativeFibonacci(){
+            
+            int n = 15;
+            int a = 0;
+            int b = 0;
+            int c = 1;
+
+            while (a < n)
+            {
+                a += 1;
+                
+                Console.Write(b + " ");
+
+                int between = b;
+                b = c;
+                c = between + c;
+            }
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
+
